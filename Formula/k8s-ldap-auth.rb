2301e04a82c0fab990bfa4fab6fe245ec4c523a3
Vianney Bouchaud <vianney@bouchaud.org>
2022-06-07 11:31:25 +0200

    passing parameters to go build

--- Formula/k8s-ldap-auth.rb
+++ Formula/k8s-ldap-auth.rb
@@ -7,27 +7,21 @@ class K8sLdapAuth < Formula
 
   head "https://github.com/vbouchaud/k8s-ldap-auth", using: :git, branch: "master"
 
-  depends_on "git" => :build
   depends_on "go" => :build
 
   def install
-    ENV["VERSION"] = version.to_s
-    ENV["COMMITHASH"] = "ef268b9a39df600b7ce0ed2652fff71fd3063ef3"
-    ENV["PKG"] = "vbouchaud/k8s-ldap-auth"
-    ENV["APPNAME"] = "k8s-ldap-auth"
-    ENV["BUILDTIME"] = DateTime.now.to_s
-    ENV["GOVERSION"] = "unsupported"
-      # system("go", "version", "|", "sed", "-e", "s/go version go\([0-9]\{1,3\}\.[0-9]\{1,3\}\).*/\1/g").to_s
+    # go version should be available through something like:
+    # system("go", "version", "|", "sed", "-e", "s/go version go\([0-9]\{1,3\}\.[0-9]\{1,3\}\).*/\1/g").to_s
 
     ENV["CGO_ENABLED"] = "0"
 
     system "go", "build", "-a", "-o", "k8s-ldap-auth",
-           "-ldflags", "-X ${PKG}/version.APPNAME=${APPNAME} \
-                        -X ${PKG}/version.VERSION=${VERSION} \
-                        -X ${PKG}/version.GOVERSION=${GOVERSION} \
-                        -X ${PKG}/version.BUILDTIME=${BUILDTIME} \
-                        -X ${PKG}/version.COMMITHASH=${COMMITHASH}"
-
+           "-ldflags", "\
+-X vbouchaud/k8s-ldap-auth/version.APPNAME=k8s-ldap-auth \
+-X vbouchaud/k8s-ldap-auth/version.VERSION=#{version} \
+-X vbouchaud/k8s-ldap-auth/version.GOVERSION=UNSUPPORTED \
+-X vbouchaud/k8s-ldap-auth/version.BUILDTIME=#{DateTime.now} \
+-X vbouchaud/k8s-ldap-auth/version.COMMITHASH=ef268b9a39df600b7ce0ed2652fff71fd3063ef3"
 
     libexec.install "k8s-ldap-auth"
     bin.install_symlink libexec/"k8s-ldap-auth"
