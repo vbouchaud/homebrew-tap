ccede3a05b1d79c684216ac3c08a08a482c9b3e0
Vianney Bouchaud <vianney@bouchaud.org>
2022-06-07 10:57:21 +0200

    set DateTime.now as string

--- Formula/k8s-ldap-auth.rb
+++ Formula/k8s-ldap-auth.rb
@@ -11,11 +11,11 @@ class K8sLdapAuth < Formula
   depends_on "go" => :build
 
   def install
-    ENV["VERSION"] = "4.0.0"
+    ENV["VERSION"] = version.to_s
     ENV["COMMITHASH"] = "ef268b9a39df600b7ce0ed2652fff71fd3063ef3"
     ENV["PKG"] = "vbouchaud/k8s-ldap-auth"
     ENV["APPNAME"] = "k8s-ldap-auth"
-    ENV["BUILDTIME"] = DateTime.now
+    ENV["BUILDTIME"] = "#{DateTime.now}"
     ENV["GOVERSION"] =
       system "go", "version", "|", "sed", "-e", "s/go version go\([0-9]\{1,3\}\.[0-9]\{1,3\}\).*/\1/g"
 
